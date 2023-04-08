be73e36d28459f1dd0050e9f25b3eaced9d0c957
followthedopamine <83159854+followthedopamine@users.noreply.github.com>
2023-04-09 09:36:45 +1200

    Limited tile dragging to one space

--- Assets/_Scripts/DragTiles.cs
+++ Assets/_Scripts/DragTiles.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -34,10 +35,31 @@ public class DragTiles : MonoBehaviour {
     draggedTilePosition = GetTilePositionUnderMouse();
   }
 
+  private Vector3Int GetNearestTileInDraggedDirection() {
+    Vector3Int targetTilePosition = GetTilePositionUnderMouse();
+    if (draggedTilePosition == targetTilePosition) return draggedTilePosition;
+    Vector3Int draggingDirection = targetTilePosition - draggedTilePosition;
+    Vector3Int absDraggingDirection = new(Math.Abs(draggingDirection.x), Math.Abs(draggingDirection.y), 0);
+    if (absDraggingDirection.x > absDraggingDirection.y) {
+      if (draggedTilePosition.x > targetTilePosition.x) {
+        return new Vector3Int(draggedTilePosition.x - 1, draggedTilePosition.y, 0);
+      } else {
+        return new Vector3Int(draggedTilePosition.x + 1, draggedTilePosition.y, 0);
+      }
+    } else {
+      if (draggedTilePosition.y > targetTilePosition.y) {
+        return new Vector3Int(draggedTilePosition.x, draggedTilePosition.y - 1, 0);
+      } else {
+        return new Vector3Int(draggedTilePosition.x, draggedTilePosition.y + 1, 0);
+      }
+    }
+  }
+
   private void SwitchTiles() {
     Tilemap levelTilemap = GameManager.Instance.levelManager.levelTilemap;
-    GameTile targetTile = GetTileUnderMouse();
-    Vector3Int targetTilePosition = GetTilePositionUnderMouse();
+    Vector3Int targetTilePosition = GetNearestTileInDraggedDirection();
+    GameTile targetTile = levelTilemap.GetTile<GameTile>(targetTilePosition);
+    if (targetTilePosition == draggedTilePosition) return;
     levelTilemap.SetTile(targetTilePosition, draggedTile);
     levelTilemap.SetTile(draggedTilePosition, targetTile);
   }
