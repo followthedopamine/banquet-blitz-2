207d7362b2e2db2930185fe700c81c8419689966
followthedopamine <83159854+followthedopamine@users.noreply.github.com>
2023-04-13 06:18:22 +1200

    Fixed a bug where goals would sometimes not be locked at 0

--- Assets/_Scripts/Miscellaneous/Goals.cs
+++ Assets/_Scripts/Miscellaneous/Goals.cs
@@ -26,32 +26,28 @@ public class Goals : MonoBehaviour {
   private void TrackScoreCompletion(List<Match> matches) {
     LevelManager levelManager = GameManager.Instance.levelManager;
     // ^1 is shorthand for last element in List
-    if (levelManager.goalRemaining[^1] <= 0) {
-      levelManager.goalRemaining[^1] = 0;
-      return;
-    }
+    if (levelManager.goalRemaining[^1] == 0) return;
     int tileCount = 0;
     foreach (Match match in matches) {
       tileCount += match.tilePositions.Count;
     }
     levelManager.goalRemaining[^1] -= tileCount * Score.SCORE_MULTIPLIER; // TODO: Consider attaching this to actual score
+    if (levelManager.goalRemaining[^1] < 0) levelManager.goalRemaining[^1] = 0;
   }
 
   private void TrackCountCompletion(List<Match> matches) {
     LevelManager levelManager = GameManager.Instance.levelManager;
 
     for (int i = 0; i < levelManager.goalTiles.Count; i++) {
-      if (levelManager.goalRemaining[i] <= 0) {
-        levelManager.goalRemaining[i] = 0;
-        return;
-      }
-
+      if (levelManager.goalRemaining[i] == 0) return;
       foreach (Match match in matches) {
         if (match.tileId == levelManager.goalTiles[i].id) {
           levelManager.goalRemaining[i] -= match.tilePositions.Count;
+          if (levelManager.goalRemaining[i] < 0) {
+            levelManager.goalRemaining[i] = 0;
+          }
         }
       }
     }
   }
-
 }
