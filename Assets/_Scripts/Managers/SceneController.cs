8ed5fffb0ef012d35ec907198bac89c02b41df0b
followthedopamine <83159854+followthedopamine@users.noreply.github.com>
2023-04-17 06:26:34 +1200

    Changed default scene to level select

--- Assets/_Scripts/Managers/SceneController.cs
+++ Assets/_Scripts/Managers/SceneController.cs
@@ -7,18 +7,31 @@ public class SceneController : MonoBehaviour {
   private const string GAME_SCENE = "Game";
   private const string UI_SCENE = "UI";
   private const string CORE_SCENE = "Core";
-  private readonly List<string> NON_LEVEL_SCENES = new() { GAME_SCENE, UI_SCENE, CORE_SCENE };
+  private const string LEVEL_SELECT_SCENE = "Level Select";
+  private readonly List<string> NON_LEVEL_SCENES = new() { GAME_SCENE, UI_SCENE, CORE_SCENE, LEVEL_SELECT_SCENE };
   private readonly List<string> LEVEL_SCENES = new() { "0001 Level", "0002 Level 2" };
 
   private void Start() {
-    // TODO: For now just load the first level as there is no main menu yet
-    LoadLevelByIndex(0);
+    // TODO: For now just load the level select as there is no main menu yet
+    // LoadLevelByIndex(0);
+    LoadLevelSelect();
+  }
+
+  private void LoadLevelSelect() {
+    UnloadAllLevelScenes();
+    UnloadPlayScenes();
   }
 
   private void LoadPlayScenes() {
     List<string> loadedScenes = GetAllLoadedScenes();
     if (!loadedScenes.Contains(GAME_SCENE)) SceneManager.LoadScene(GAME_SCENE, LoadSceneMode.Additive);
     if (!loadedScenes.Contains(UI_SCENE)) SceneManager.LoadScene(UI_SCENE, LoadSceneMode.Additive);
+  }
+
+  private void UnloadPlayScenes() {
+    List<string> loadedScenes = GetAllLoadedScenes();
+    if (loadedScenes.Contains(GAME_SCENE)) SceneManager.UnloadSceneAsync(GAME_SCENE);
+    if (loadedScenes.Contains(UI_SCENE)) SceneManager.UnloadSceneAsync(UI_SCENE);
   }
 
   private void LoadLevelByIndex(int index) {
