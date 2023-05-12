32233493c5902b4e4c9d0fb1701ff78f22b52c8b
followthedopamine <83159854+followthedopamine@users.noreply.github.com>
2023-05-12 14:06:55 +1200

    Created script for scaling UI elements (only works for World 1 image so far)

---
+++ Assets/_Scripts/UI/ResponsiveScaling.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ResponsiveScaling : MonoBehaviour {
+  private enum Operation {
+    ScaleToFit,
+    ScaleToFitWidth,
+  }
+
+  [SerializeField] private Operation operation;
+  private float trueImageHeight;
+  private float trueImageWidth;
+
+  private void Start() {
+    Image image = gameObject.GetComponent<Image>();
+    trueImageHeight = image.sprite.rect.height;
+    trueImageWidth = image.sprite.rect.width;
+    Debug.Log(trueImageWidth);
+    if (operation == Operation.ScaleToFitWidth) ScaleToFitWidth();
+  }
+
+  private void ScaleToFitWidth() {
+    RectTransform rect = gameObject.GetComponent<RectTransform>();
+    float scaleFactor = Screen.width / trueImageWidth;
+    rect.sizeDelta = new Vector2(Screen.width, trueImageHeight * scaleFactor);
+  }
+}
