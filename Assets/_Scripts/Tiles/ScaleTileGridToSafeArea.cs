e0928aba0a8595f38d026ceba1197adc459df64e
followthedopamine <83159854+followthedopamine@users.noreply.github.com>
2023-05-13 09:10:32 +1200

    Created tilemap scaling script

---
+++ Assets/_Scripts/Tiles/ScaleTileGridToSafeArea.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScaleTileGridToSafeArea : MonoBehaviour {
+  // Safe area percentages
+  private float safeHeight;
+  private float safeWidth;
+
+  // Goals of this script:
+  // Calculate true size of combined tiles in tilemap
+  // Scale tilemap to fill the safe area as best as possible
+}
